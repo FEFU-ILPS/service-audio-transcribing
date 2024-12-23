59ff477af7761e8ea6dd0384c6b4405934352ed1
Terekhin Bogdan <yato050303@gmail.com>
2024-12-23 14:34:32 +1000

    init django project

---
+++ voice_model_service/voice_model_service/settings.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'rest_framework',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'voice_model_service.urls'
+
+WSGI_APPLICATION = 'voice_model_service.wsgi.application'
+
+ASGI_APPLICATION = 'voice_model_service.asgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
