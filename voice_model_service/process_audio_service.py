d2d1fd6075db6addaaeafd2a083f4da748beae33
Terekhin Bogdan <yato050303@gmail.com>
2025-01-08 22:45:12 +1000

    configure swagger

--- voice_model_service/process_audio_service.py
+++ voice_model_service/process_audio_service.py
@@ -2,7 +2,7 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 import torch
 
 
-def process_audio(audio_input, sample_rate):
+def process_audio(audio_input, sample_rate) -> list[str]:
     local_path = "/voice_model_service/voice_model"
     processor = Wav2Vec2Processor.from_pretrained(local_path)
     model = Wav2Vec2ForCTC.from_pretrained(local_path)
@@ -12,7 +12,7 @@ def process_audio(audio_input, sample_rate):
 
     with torch.no_grad():
         logits = model(input_values).logits
- 
+
     predicted_ids = torch.argmax(logits, dim=-1)
     transcription = processor.batch_decode(predicted_ids)
     return transcription
