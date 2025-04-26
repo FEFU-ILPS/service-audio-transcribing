5f986a86239da1ca2418ba66b5795a48a75324e6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-27 00:16:04 +1000

    feature, refactor: Приложение FastAPI с предзакгрузкой моделей

---
+++ app.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from configs import configs
+from models import model_manager
+from routers import transcribing_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on_startup
+    await model_manager.load_models(
+        rm_files=not configs.DEBUG_MODE,
+    )
+
+    yield
+
+    # on_shutdown
+
+
+service = FastAPI(lifespan=lifespan)
+
+service.include_router(transcribing_router)
