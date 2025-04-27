f476df554c45b19bb4f76c644ed18f8bf9eb032b
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-27 18:01:41 +1000

    feature: Проверка свойств файла

--- routers/transcribing.py
+++ routers/transcribing.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Annotated
 
 import soundfile as sf
-from fastapi import APIRouter, File, Form, UploadFile, HTTPException, status
+from fastapi import APIRouter, File, Form, HTTPException, UploadFile, status
 
 from models import model_manager
 from schemas import TranscribingResponse
@@ -28,8 +28,20 @@ async def transcribe(
             detail=f"The model for the language '{lang.value}' is not uploaded in the service.",
         )
 
-    file_bytestream = file.file
-    audio_input, sample_rate = sf.read(file_bytestream)
+    if not file.filename.lower().endswith(".wav"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid file extension. Only files with the extension '.wav' are allowed.",
+        )
+
+    if file.content_type not in ["audio/wav", None]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid content type. Only audio/wav is allowed for WAV files.",
+        )
+
+    file_binarystream = file.file
+    audio_input, sample_rate = sf.read(file_binarystream)
 
     transcription = transcribe_audio(audio_input, sample_rate, lang.value)
 
