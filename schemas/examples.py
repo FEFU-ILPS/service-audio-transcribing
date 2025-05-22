f8271278175237488426ffb542a6e1b4e043350b
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-22 21:21:52 +1000

    refactor: Рефактор схем Pydantic (#18)

--- schemas/examples.py
+++ schemas/examples.py
@@ -1,4 +1,6 @@
 TRANSCRIPTIONS_EXAMPLE = [
-    "Dɑːɹk hæd hæd jɚ jɚ ʃi suːt suːt ʃi hæd jɚ jɚ ʃi",
-    "hæd suːt jɚ jɚ ʃi dɑːɹk ʃi hæd ʃi dɑːɹk ʃi hæd hæd hæd suːt jɚ suːt",
+    "r ɔɪ ɪ ˈnɔː r d ð ə ˈspjʊ r i ə s ˈdeɪ t ə pɔɪ n t s ɪ n ˈdɹɔː ɪ ŋ ð ə ɡ r æ f",
+    "aɪ ə ˈsuː m ˈmɔɪ s t ʃ ɚ w ɪ l ˈdæ m ɪ dʒ ð ɪ s ʃ ɪ p s h ʌ l",
+    "ɑː r ˈhɑː l ɪ d eɪ ˈeɪ p r ɑː n z ə ˈveɪ l ə b l t ə ə s",
+    "ˈɔː l m oʊ s t ɔː l ˈkɑː l ɪ dʒ ɪ z ɑː r naʊ ˌkoʊ ɛ dʒ ʊ ˈkeɪ ʃ n l",
 ]
--- schemas/transcribing.py
+++ schemas/transcribing.py
@@ -1,5 +1,3 @@
-from typing import Annotated
-
 from pydantic import BaseModel, Field
 
 from .examples import TRANSCRIPTIONS_EXAMPLE
@@ -8,6 +6,4 @@ from .examples import TRANSCRIPTIONS_EXAMPLE
 class TranscribingResponse(BaseModel):
     """Данные, отправляемые после транскрибирования речи из файла."""
 
-    transcription: Annotated[
-        str, Field(description="Транскрипция речи", examples=TRANSCRIPTIONS_EXAMPLE)
-    ]
+    transcription: str = Field(description="Транскрипция речи", examples=TRANSCRIPTIONS_EXAMPLE)
